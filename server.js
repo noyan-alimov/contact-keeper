ebe47123261ea499f2aee611a262a97cd5ebe813
Noyan Alimov <noyan.alimov@gmail.com>
2020-04-09 19:40:34 +0100

    finished back end work, finished users and contacts routes

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 // Define routes
 app.use("/api/users", require("./routes/users"));
